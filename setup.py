8830afe30cb77f64d018b615bc6b2b8fae199958
Malte Schütze <malte.schuetze@fgms.de>
2017-03-04 15:42:46 +0100

    Fix setup.py module folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lookingglass",
-    packages=["lookingglass"],
+    packages=["frontend"],
     include_package_data=True,
     version="0.0.1",
     install_requires=[
